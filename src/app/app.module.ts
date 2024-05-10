e6b0ba2991488bdff4f9deb17d6d288e6b6a5ea9
v.artemyeva <v.i.artemyeva@yandex.ru>
2024-05-10 15:23:24 +0300

    install Nebular (UI-kit)
    installed and basic setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbButtonModule, NbLayoutModule, NbThemeModule } from '@nebular/theme';
 
 @NgModule({
 	declarations: [AppComponent],
 	imports: [
 		BrowserModule,
 		BrowserAnimationsModule,
+		NbThemeModule.forRoot({ name: 'cosmic' }),
+		NbLayoutModule,
+		NbButtonModule,
 	],
 	bootstrap: [AppComponent],
 	exports: [],
